336264b6a870198b03b4422b19372beca3e8816e
PauloBaronista <paulo@rpm.com.br>
2022-02-07 20:36:15 -0300

    https

--- assets/js/jFuncoes.js
+++ assets/js/jFuncoes.js
@@ -95,7 +95,7 @@ $(document).on('ready', function () {
 $(document).ready(function () {
     $("#close").click(function () {
         $("#aviso").hide();
-        window.location.href = "http://reservaelementum.com.br/"
+        window.location.href = "https://www.reservaelementum.com.br/"
     });
 });
 
